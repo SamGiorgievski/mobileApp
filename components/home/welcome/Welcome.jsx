8f0a5998341999b3459aff0e16dc5addc0cf0228
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-11-17 13:26:28 -0500

    search funct

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+const guideTypes = ["Guides", "Services", "Recipes"];
 
-const Welcome = () => {
+const Welcome = ( { searchTerm, setSearchTerm, handleClick }) => {
+
+  const router = useRouter();
+  const [activeGuideType, setActiveGuideType] = useState("Guides");
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Welcome Sam!</Text>
+        <Text style={styles.welcomeMessage}>What do you need help with?</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={guideTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeGuideType, item)}
+              onPress={() => {
+                setActiveGuideType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeGuideType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+
     </View>
   )
 }
