a468f0a71a90314fdb9ccb04fea0cfc7b06e91de
NFerracuti <npferracuti@gmail.com>
2023-12-22 23:48:59 -0500

    add api directory and move forum data

---
+++ api/forum.js
@@ -0,0 +1,15 @@
+const forumPlaceholderData = [
+  { id: 1, title: 'The Latest Research on Celiac Disease: Updates and Insights', lastCommentDate: new Date(2023, 11, 10) },
+  { id: 2, title: 'Top 10 Gluten-Free Snacks for Busy Days', lastCommentDate: new Date(2023, 10, 5) },
+  { id: 3, title: 'Best Gluten-Free Restaurants in Toronto: 2023 Edition', lastCommentDate: new Date(2023, 11, 5) },
+  { id: 4, title: 'Navigating Social Events with Celiac Disease: Tips and Tricks', lastCommentDate: new Date(2023, 10, 7) },
+  { id: 5, title: 'Gluten-Free Recipe Exchange', lastCommentDate: new Date(2023, 10, 8) },
+  { id: 6, title: 'Finding the Right Gluten-Free Resources: Books, Websites, and Apps', lastCommentDate: new Date(2023, 10, 10) },
+  { id: 7, title: 'Traveling with Celiac Disease: How to Stay Gluten-Free on the Road', lastCommentDate: new Date(2023, 10, 5) },
+  { id: 8, title: 'Understanding Cross-Contamination: How to Keep Your Kitchen Safe', lastCommentDate: new Date(2023, 6, 5) },
+  { id: 9, title: 'Traveling with Celiac Disease: How to Stay Gluten-Free on the Road', lastCommentDate: new Date(2023, 1, 5) },
+  { id: 10, title: 'Gluten-Free Kids: Support and Ideas for Parents', lastCommentDate: new Date(2023, 2, 5) },
+  { id: 11, title: 'Gluten-Free Grocery Shopping: Finding the Best Products and Deals', lastCommentDate: new Date(2023, 2, 5) },
+];
+
+export default forumPlaceholderData;
--- app/(tabs)/forum.js
+++ app/(tabs)/forum.js
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Stack, useRouter } from 'expo-router';
 import { COLORS, icons, images, SIZES } from '../../constants';
+import forumPlaceholderData from '../../api/forum';
 import {
   Guides,
   ScreenHeaderBtn,
@@ -15,19 +16,7 @@ export default function camera() {
 
   const router = useRouter();
   // Mock forum topics data
-  const [forumTopics, setForumTopics] = useState([
-    { id: 1, title: 'The Latest Research on Celiac Disease: Updates and Insights', lastCommentDate: new Date(2023, 11, 10) },
-    { id: 2, title: 'Top 10 Gluten-Free Snacks for Busy Days', lastCommentDate: new Date(2023, 10, 5) },
-    { id: 3, title: 'Best Gluten-Free Restaurants in Toronto: 2023 Edition', lastCommentDate: new Date(2023, 11, 5) },
-    { id: 4, title: 'Navigating Social Events with Celiac Disease: Tips and Tricks', lastCommentDate: new Date(2023, 10, 7) },
-    { id: 5, title: 'Gluten-Free Recipe Exchange', lastCommentDate: new Date(2023, 10, 8) },
-    { id: 6, title: 'Finding the Right Gluten-Free Resources: Books, Websites, and Apps', lastCommentDate: new Date(2023, 10, 10) },
-    { id: 7, title: 'Traveling with Celiac Disease: How to Stay Gluten-Free on the Road', lastCommentDate: new Date(2023, 10, 5) },
-    { id: 8, title: 'Understanding Cross-Contamination: How to Keep Your Kitchen Safe', lastCommentDate: new Date(2023, 6, 5) },
-    { id: 9, title: 'Traveling with Celiac Disease: How to Stay Gluten-Free on the Road', lastCommentDate: new Date(2023, 1, 5) },
-    { id: 10, title: 'Gluten-Free Kids: Support and Ideas for Parents', lastCommentDate: new Date(2023, 2, 5) },
-    { id: 11, title: 'Gluten-Free Grocery Shopping: Finding the Best Products and Deals', lastCommentDate: new Date(2023, 2, 5) },
-  ]);
+  const [forumTopics, setForumTopics] = useState(forumPlaceholderData);
 
   // Sort topics by most recent comment
   const sortedTopics = forumTopics.sort((a, b) => b.lastCommentDate - a.lastCommentDate);
